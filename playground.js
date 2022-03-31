653f41cc76ebf0408261ca524bb4f25b55e302b4
Lesley Cheung <lesley.yc@gmail.com>
2022-04-01 04:16:09 +0800

    first commit

---
+++ playground.js
@@ -0,0 +1,23 @@
+const samo = require("samo")
+
+// will create folder structure for you
+samo.init()
+
+// print out the hbs templates in code clarity 
+samo.support()
+
+// variables 
+samo.perspective()
+
+let DB_NAME = "DB_NAME"
+
+function simplify() {
+    let string = `
+  DB_NAME = for_my_friends
+  DB_PASSWORD = 
+
+  FEATURE
+  `
+}
+
+samo.list()
